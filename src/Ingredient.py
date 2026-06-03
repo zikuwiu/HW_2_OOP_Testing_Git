528bf138df38b6fe7325d90fd3bef73d3a27fbac
stepunk6 <113301798+stepunk6@users.noreply.github.com>
2026-06-03 20:53:10 +0300

    feat: add Ingredient.py

---
+++ src/Ingredient.py
@@ -0,0 +1,23 @@
+class Ingredient:
+    def __init__(self, name, quantity, unit):
+        self.name = name
+        self._quantity = quantity
+        self.unit = unit
+    @property
+    def quantity(self):
+        return self._quantity
+    @quantity.setter
+    def quantity(self, quantity):
+        if quantity <= 0:
+            raise ValueError("Количество должно быть положительным")
+        self._quantity = float(quantity)
+    def __str__(self):
+        return f"{self.name}: {self.quantity} {self.unit}"
+    def __repr__(self):
+        return f"Ingredient('{self.name}', {self.quantity}, '{self.unit}')"
+    def __eq__(self, other):
+        if isinstance(other, Ingredient):
+            raise TypeError("Сравнивать можно только экземпляры одного класса")
+        if self.name == other.name and self.unit == other.unit:
+            return True
+        return False

e221a23edb20e39015a747eafee6988d2dfb83eb
stepunk6 <113301798+stepunk6@users.noreply.github.com>
2026-06-03 21:31:10 +0300

    fix: safety __init__ style: fix whitespaces

--- src/Ingredient.py
+++ src/Ingredient.py
@@ -1,20 +1,25 @@
 class Ingredient:
-    def __init__(self, name, quantity, unit):
+    def __init__(self, name: str, quantity: float, unit: str):
         self.name = name
         self._quantity = quantity
         self.unit = unit
+
     @property
     def quantity(self):
         return self._quantity
+
     @quantity.setter
     def quantity(self, quantity):
         if quantity <= 0:
             raise ValueError("Количество должно быть положительным")
         self._quantity = float(quantity)
+
     def __str__(self):
         return f"{self.name}: {self.quantity} {self.unit}"
+
     def __repr__(self):
         return f"Ingredient('{self.name}', {self.quantity}, '{self.unit}')"
+
     def __eq__(self, other):
         if isinstance(other, Ingredient):
             raise TypeError("Сравнивать можно только экземпляры одного класса")
